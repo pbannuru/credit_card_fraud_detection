1699e07e01e08016e879aef11770e0229ab85bcd
pbannuru <pbannuru@gmail.com>
2022-08-19 21:50:16 +0530

    updated

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask,render_template
-
+from flask import Flask
+from martsales.logger import logging
+from martsales.exception import martsalesException
+import sys
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 
 def index():
+    try:
+        raise Exception("we are testing Exception")
+        logging.info("exception test")
+    except Exception as e:
+        martsales=martsalesException(e,sys)
+        logging.info(martsales.error_message)
+        logging.info("this logging test")
     return "starting Machine learning"
 
 
