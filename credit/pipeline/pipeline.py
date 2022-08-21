757f7a93e6a1d85d984caa01cb92f8cfde914457
pbannuru <pbannuru@gmail.com>
2022-08-21 21:44:12 +0530

    updated

--- credit/pipeline/pipeline.py
+++ credit/pipeline/pipeline.py
@@ -2,10 +2,15 @@
 from credit.config.configuration import Configuartion
 from credit.logger import logging, get_log_file_name
 from credit.exception import creditException
-from credit.entity.artifact_entity import ModelPusherArtifact, DataIngestionArtifact, ModelEvaluationArtifact
-from credit.entity.artifact_entity import DataValidationArtifact, DataTransformationArtifact, ModelTrainerArtifact
-from credit.entity.config_entity import DataIngestionConfig, ModelEvaluationConfig
+from credit.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, \
+    DataTransformationArtifact,ModelTrainerArtifact,ModelEvaluationArtifact,ModelPusherArtifact
 from credit.component.data_ingestion import DataIngestion
+from credit.component.data_validation import DataValidation
+from credit.component.data_transformation import DataTransformation
+from credit.component.data_ingestion import DataIngestion
+from credit.component.model_trainer import ModelTrainer
+from credit.component.model_evaluation import ModelEvaluation
+from credit.component.model_pusher import ModelPusher
 import os, sys
 import pandas as pd
 
@@ -26,11 +31,72 @@ class Pipeline:
             return data_ingestion.initiate_data_ingestion()
         except Exception as e:
             raise creditException(e, sys) from e
+    
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) \
+            -> DataValidationArtifact:
+        try:
+            data_validation = DataValidation(data_validation_config=self.config.get_data_validation_config(),
+                                             data_ingestion_artifact=data_ingestion_artifact
+                                             )
+            return data_validation.initiate_data_validation()
+        except Exception as e:
+            raise creditException(e, sys) from e 
 
     def run_pipeline(self):
         try:
             #data_ingestion 
 
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_artifact=data_validation_artifact
+            )
+        except Exception as e:
+            raise creditException(e, sys) from e
+
+    def start_data_transformation(self,
+                                  data_ingestion_artifact: DataIngestionArtifact,
+                                  data_validation_artifact: DataValidationArtifact
+                                  ) -> DataTransformationArtifact:
+        try:
+            data_transformation = DataTransformation(
+                data_transformation_config=self.config.get_data_transformation_config(),
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_artifact=data_validation_artifact
+            )
+            return data_transformation.initiate_data_transformation()
+        except Exception as e:
+            raise creditException(e, sys)
+
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(model_trainer_config=self.config.get_model_trainer_config(),
+                                         data_transformation_artifact=data_transformation_artifact
+                                         )
+            return model_trainer.initiate_model_trainer()
+        except Exception as e:
+            raise creditException(e, sys) from e
+
+    def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
+                               data_validation_artifact: DataValidationArtifact,
+                               model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
+        try:
+            model_eval = ModelEvaluation(
+                model_evaluation_config=self.config.get_model_evaluation_config(),
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_artifact=data_validation_artifact,
+                model_trainer_artifact=model_trainer_artifact)
+            return model_eval.initiate_model_evaluation()
+        except Exception as e:
+            raise creditException(e, sys) from e
+
+    def start_model_pusher(self, model_eval_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
+        try:
+            model_pusher = ModelPusher(
+                model_pusher_config=self.config.get_model_pusher_config(),
+                model_evaluation_artifact=model_eval_artifact
+            )
+            return model_pusher.initiate_model_pusher()
         except Exception as e:
             raise creditException(e, sys) from e
--- demo.py
+++ demo.py
@@ -2,11 +2,13 @@ from credit.pipeline.pipeline import Pipeline
 from credit.exception import creditException
 from credit.config import configuration
 from credit.logger import logging
+from credit.config.configuration import *
 def main():
     try:
         pipeline =Pipeline()
-
         pipeline.run_pipeline()
+        # data_validation_config = configuration().get_data_validation_config()
+        # print(data_validation_config)
 
     except Exception as e:
         logging.error(f"{e}")
