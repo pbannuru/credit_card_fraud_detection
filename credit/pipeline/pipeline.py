0139a9748975073fbc3aed3cf5242cc872657574
pbannuru <pbannuru@gmail.com>
2022-08-22 19:12:27 +0530

    modeltrained

--- credit/pipeline/pipeline.py
+++ credit/pipeline/pipeline.py
@@ -52,6 +52,18 @@ class Pipeline:
                 data_ingestion_artifact=data_ingestion_artifact,
                 data_validation_artifact=data_validation_artifact
             )
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
+                                                                    data_validation_artifact=data_validation_artifact,
+                                                                    model_trainer_artifact=model_trainer_artifact)
+    
+            if model_evaluation_artifact.is_model_accepted:
+                model_pusher_artifact = self.start_model_pusher(model_eval_artifact=model_evaluation_artifact)
+                logging.info(f'Model pusher artifact: {model_pusher_artifact}')
+            else:
+                logging.info("Trained model rejected.")
+            
+            logging.info("Pipeline completed.")
         except Exception as e:
             raise creditException(e, sys) from e
 
